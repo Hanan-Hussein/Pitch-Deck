681b3e48198cf6b846c00265f2a7913aec5e65e6
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-10 01:46:42 +0300

    ft:register-specifying register route

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,20 @@ def home():
     pitches = Pitch.query.all()
     
     return render_template('index.html',pitches=pitches)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = Register()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your Account Has been Created! You are now able to login  in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
