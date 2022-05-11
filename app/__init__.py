8e540c1700af19efd44be1c05e79b1b0c7e41699
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-11 11:33:02 +0300

    mail-added a configuration for email setup

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = '37183a998ba83a7b481bd3f6e0f2b29'
-
+# the three /// are the relative path from the current file
+# site.db file should get created in the project directory along side py module
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 
 db = SQLAlchemy(app)
+mail=Mail(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 login_manager.login_message_category = 'info'
+from . import routes
